ee1ddac0d54548750a96e557a5aaa8ebfb56b3e8
ayushpuranik <apuranik.09@gmail.com>
2021-01-05 22:19:49 +0530

    Adding inmemory user. (won't work due to missing password encoder)

--- src/main/java/com/example/springsecuritydemo/config/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/ApplicationSecurityConfig.java
@@ -2,10 +2,15 @@ package com.example.springsecuritydemo.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -13,14 +18,30 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationSecurityConfig.class);
 
+
+    //Drawback : can not logout as authentication is provided with every request.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         LOG.info("Updating Security Configs");
         http
                 .authorizeRequests()
+                //permit few urls to all users
+                .antMatchers("/", "index"/*,"/css/*","/js/*"*/)
+                .permitAll()
                 .anyRequest()       // any request must
                 .authenticated()    // be authenticated
                 .and()
                 .httpBasic();       // basic authentication
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("user1")
+                .password("password")
+                .roles("Student")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
     }
 }
