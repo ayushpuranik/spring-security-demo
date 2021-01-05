1b4dc9319ac469d75cfaa001dfe58ea7c7bc61b6
ayushpuranik <apuranik.09@gmail.com>
2021-01-05 17:45:53 +0530

    Basic Authentication added to validate any request

--- src/main/java/com/example/springsecuritydemo/config/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springsecuritydemo.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,10 +13,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationSecurityConfig.class);
 
-    /*@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         LOG.info("Updating Security Configs");
-
-
-    }*/
+        http
+                .authorizeRequests()
+                .anyRequest()       // any request must
+                .authenticated()    // be authenticated
+                .and()
+                .httpBasic();       // basic authentication
+    }
 }
